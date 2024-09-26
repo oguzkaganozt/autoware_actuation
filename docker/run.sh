4fb4c4bffbbb6266b294864bf8ec4057c76b3d9c
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-09-26 20:19:37 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 xhost +
-docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host -v /dev/shm:/dev/shm -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp ghcr.io/autowarefoundation/openadkit_demo.autoware:aws-reinvent-simulator-monolithic-amd64 ros2 launch scenario_test_runner scenario_test_runner.launch.py   architecture_type:=awf/universe/20240605   record:=false   scenario:='$(find-pkg-share scenario_test_runner)/scenario/sample.yaml'   sensor_model:=sample_sensor_kit   vehicle_model:=sample_vehicle initialize_duration:=90
+
+docker run -it --net=host -v /dev/shm:/dev/shm -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -e ROS_DOMAIN_ID=99 \
+    -e ROS_LOCALHOST_ONLY=0 \
+    ghcr.io/autowarefoundation/autoware:universe-amd64 ros2 launch autoware_launch planning_simulator.launch.xml \
+    map_path:=/opt/autoware/share/kashiwanoha_map \
+    vehicle_model:=sample_vehicle \
+    sensor_model:=sample_sensor_kit \
+    scenario_simulation:=true \
+    rviz:=false
