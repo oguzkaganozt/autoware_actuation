90bd813f184592d505ddacd58fceeccf71c21e77
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-09-27 22:11:03 +0300

    add docker compose file
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -2,12 +2,4 @@
 
 xhost +
 
-docker run -it --net=host -v /dev/shm:/dev/shm -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
-    -e ROS_DOMAIN_ID=99 \
-    -e ROS_LOCALHOST_ONLY=0 \
-    ghcr.io/autowarefoundation/autoware:universe-amd64 ros2 launch autoware_launch planning_simulator.launch.xml \
-    map_path:=/opt/autoware/share/kashiwanoha_map \
-    vehicle_model:=sample_vehicle \
-    sensor_model:=sample_sensor_kit \
-    scenario_simulation:=true \
-    rviz:=false
+docker compose -f docker-compose.yml up
