4c9424050f747a22c8d25690866b9eb20c2791aa
Oguz <oguzkaganozt@gmail.com>
2024-12-02 13:37:58 +0000

    .

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-xhost +
+#xhost +
 
 # Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source ./simulator.env
+TIMEOUT=60 CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up web-visualizer -d
 # Use the script directory to make the docker-compose.yml path relative
-docker compose -f "$SCRIPT_DIR/docker-compose.yml" up
+
+while true; do
+    	echo "Updating planning container.."
+        echo "Running planning v1.."
+	TIMEOUT=70 CONF_FILE=$CONF_FILE_FAIL docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
+        echo "Updating planning container.."
+        echo "Running planning v2.."
+	TIMEOUT=120 CONF_FILE=$CONF_FILE_PASS docker compose -f "$SCRIPT_DIR/docker-compose.yml" up planning-control simulator --abort-on-container-exit
+done
